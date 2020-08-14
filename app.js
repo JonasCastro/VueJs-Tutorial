41c823768b976ace1c5048cf265e535ad85e0220
Jonas Castro <jonascastro128@gmail.com>
2020-08-14 11:02:03 -0300

    data binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el:'#vue-app',
     data: {
         name: 'Jonas',
-        job:"Dev"
+        job:"Dev",
+        website:'https://vuejs.org/',
+        websiteTag:'<a href="https://vuejs.org/">vuejs</a>'
       },
       methods:{
         greet:function(time){
