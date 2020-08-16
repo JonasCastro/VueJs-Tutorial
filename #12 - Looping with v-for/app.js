763f30c505d6a6454fff98235aff83a3a5e7dafa
Jonas Castro <jonascastro128@gmail.com>
2020-08-16 09:41:18 -0300

    Looping with v-for

---
+++ #12 - Looping with v-for/app.js
@@ -0,0 +1,12 @@
+new Vue({
+  el:'#vue-app',
+  data: {
+      characters2: {name:"Ryu", age:25, name2:"Ryu", age2:25},
+      characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+      ninjas:[        
+        {name:"Ryu", age:25},
+        {name:"Yoshi", age:25},
+        {name:"Ken", age:25},
+      ]
+    },
+})
